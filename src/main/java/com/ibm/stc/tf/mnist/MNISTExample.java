8fb9b909f78ab0a42d076d0243614506276fc356
Deron Eriksson <deron@us.ibm.com>
2018-03-19 16:59:39 -0700

    Try out MetaGraphDef

--- src/main/java/com/ibm/stc/tf/mnist/MNISTExample.java
+++ src/main/java/com/ibm/stc/tf/mnist/MNISTExample.java
@@ -3,13 +3,25 @@ package com.ibm.stc.tf.mnist;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import org.deeplearning4j.datasets.mnist.MnistManager;
+import org.tensorflow.DataType;
+import org.tensorflow.Graph;
+import org.tensorflow.Operation;
+import org.tensorflow.Output;
 import org.tensorflow.SavedModelBundle;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.framework.MetaGraphDef;
+import org.tensorflow.framework.SignatureDef;
 
 /**
  * Try an MNIST saved model created in Tensorflow from Java.
@@ -20,34 +32,55 @@ import org.tensorflow.SavedModelBundle;
 public class MNISTExample {
 
 	public static final String MNIST_DATA_DIR = "/tmp/mnist_data/";
-	public static final String MNIST_SAVED_MODEL_DIR = "/tmp/mnist_saved_model/";
+	public static final String MNIST_SAVED_MODEL_DIR = "./model/";
 
 	public static final String TRAIN_IMAGES = "train-images-idx3-ubyte";
 	public static final String TRAIN_LABELS = "train-labels-idx1-ubyte";
 	public static final String TEST_IMAGES = "t10k-images-idx3-ubyte";
 	public static final String TEST_LABELS = "t10k-labels-idx1-ubyte";
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		try {
 
-		String savedModelDir = MNIST_SAVED_MODEL_DIR + "1521072365";
-		SavedModelBundle savedModel = SavedModelBundle.load(savedModelDir, "serve");
+			String savedModelDir = MNIST_SAVED_MODEL_DIR; // + "1521072365";
+			SavedModelBundle savedModel = SavedModelBundle.load(savedModelDir, "serve");
 
-		MnistManager trainingManager = getTrainingManager();
-		trainingManager.setCurrent(0);
-		int label = trainingManager.readLabel();
-		System.out.println("Label: " + label);
-		int[][] image = trainingManager.readImage();
-		displayImageAsText(image);
-		displayImage(image);
+			// MnistManager trainingManager = getTrainingManager();
+			// trainingManager.setCurrent(0);
+			// int label = trainingManager.readLabel();
+			// System.out.println("Label: " + label);
+			// int[][] image = trainingManager.readImage();
+			// displayImageAsText(image);
+			// displayImage(image);
 
-		MnistManager testManager = getTestManager();
-		testManager.setCurrent(0);
-		label = trainingManager.readLabel();
-		System.out.println("Label: " + label);
-		image = trainingManager.readImage();
-		displayImageAsText(image);
-		displayImage(image);
+			MnistManager testManager = getTestManager();
+			testManager.setCurrent(0);
+			int label = testManager.readLabel();
+			System.out.println("Label: " + label);
+			int[][] iImage = testManager.readImage();
+			displayImageAsText(iImage);
+			displayImage(iImage);
 
+			float[][] image = iToF(iImage);
+
+			Graph g = savedModel.graph();
+			Iterator<Operation> operations = g.operations();
+			while (operations.hasNext()) {
+				Operation op = operations.next();
+				System.out.println("OP:" + op);
+			}
+
+			byte[] metaGraphDefBytes = savedModel.metaGraphDef();
+			MetaGraphDef mgd = MetaGraphDef.parseFrom(metaGraphDefBytes);
+			Map<String, SignatureDef> signatureDefMap = mgd.getSignatureDefMap();
+			Set<Entry<String, SignatureDef>> entries = signatureDefMap.entrySet();
+			for (Entry<String, SignatureDef> entry : entries) {
+				System.out.println("ENTRY:" + entry);
+			}
+
+		} catch (Throwable t) {
+			System.out.println(t);
+		}
 	}
 
 	public static MnistManager getTrainingManager() throws IOException {
@@ -87,6 +120,16 @@ public class MNISTExample {
 			sb.append("\n");
 		}
 		System.out.println(sb.toString());
+	}
+
+	public static float[][] iToF(int[][] image) {
+		float[][] fImage = new float[image.length][image[0].length];
+		for (int r = 0; r < image.length; r++) {
+			for (int c = 0; c < image[0].length; c++) {
+				fImage[r][c] = image[r][c];
+			}
+		}
+		return fImage;
 	}
 
 	public static void displayImage(int[][] image) {
