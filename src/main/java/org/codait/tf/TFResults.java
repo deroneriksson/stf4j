baed846f33ebc6f374e540e17bc86e2cc386e224
Deron Eriksson <deron@us.ibm.com>
2018-04-11 16:51:44 -0700

    Add additional Dtype checks

--- src/main/java/org/codait/tf/TFResults.java
+++ src/main/java/org/codait/tf/TFResults.java
@@ -85,12 +85,18 @@ public class TFResults {
 
 	public Object getLongArrayMultidimensional(String key) {
 		checkKey(key);
-		@SuppressWarnings("unchecked")
-		Tensor<Long> tensor = (Tensor<Long>) outputNameToValue.get(outputKeyToName.get(key));
-		int[] shape = ArrayUtil.lToI(tensor.shape());
-		Object dest = Array.newInstance(long.class, shape);
-		tensor.copyTo(dest);
-		return dest;
+		TensorInfo ti = TFUtil.outputKeyToTensorInfo(key, model.metaGraphDef());
+		if (ti.getDtype() == DataType.DT_INT64) {
+			@SuppressWarnings("unchecked")
+			Tensor<Long> tensor = (Tensor<Long>) outputNameToValue.get(outputKeyToName.get(key));
+			int[] shape = ArrayUtil.lToI(tensor.shape());
+			Object dest = Array.newInstance(long.class, shape);
+			tensor.copyTo(dest);
+			return dest;
+		} else {
+			throw new TFException(
+					"getLongArrayMultidimensional not implemented for '" + key + "' data type: " + ti.getDtype());
+		}
 	}
 
 	public float[] getFloatArray(String key) {
@@ -109,12 +115,18 @@ public class TFResults {
 
 	public Object getFloatArrayMultidimensional(String key) {
 		checkKey(key);
-		@SuppressWarnings("unchecked")
-		Tensor<Float> tensor = (Tensor<Float>) outputNameToValue.get(outputKeyToName.get(key));
-		int[] shape = ArrayUtil.lToI(tensor.shape());
-		Object dest = Array.newInstance(float.class, shape);
-		tensor.copyTo(dest);
-		return dest;
+		TensorInfo ti = TFUtil.outputKeyToTensorInfo(key, model.metaGraphDef());
+		if (ti.getDtype() == DataType.DT_FLOAT) {
+			@SuppressWarnings("unchecked")
+			Tensor<Float> tensor = (Tensor<Float>) outputNameToValue.get(outputKeyToName.get(key));
+			int[] shape = ArrayUtil.lToI(tensor.shape());
+			Object dest = Array.newInstance(float.class, shape);
+			tensor.copyTo(dest);
+			return dest;
+		} else {
+			throw new TFException(
+					"getFloatArrayMultidimensional not implemented for '" + key + "' data type: " + ti.getDtype());
+		}
 	}
 
 	public int getInt(String key) {
