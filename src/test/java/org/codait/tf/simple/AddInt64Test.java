365add62f3ef6ad4087727617828fd706d98418f
Deron Eriksson <deron@us.ibm.com>
2018-08-02 18:20:58 -0700

    AddInt64 multidim inputs and multidim outputs tests

--- src/test/java/org/codait/tf/simple/AddInt64Test.java
+++ src/test/java/org/codait/tf/simple/AddInt64Test.java
@@ -150,4 +150,64 @@ public class AddInt64Test {
 				.out("output").run().getStringArray("output");
 		Assert.assertArrayEquals(new String[] { "4", "6" }, result);
 	}
+
+	@Test
+	public void inputMultiLongArraysOutputMultiLongArray() {
+		long[][] input1 = new long[][] { { 1L, 2L }, { 3L, 4L } };
+		long[][] input2 = new long[][] { { 1L, 2L }, { 3L, 4L } };
+		long[][] expected = new long[][] { { 2L, 4L }, { 6L, 8L } };
+		long[][] result = (long[][]) model.in("input1", input1).in("input2", input2).out("output").run()
+				.getLongArrayMultidimensional("output");
+		for (int i = 0; i < expected.length; i++) {
+			Assert.assertArrayEquals(expected[i], result[i]);
+		}
+	}
+
+	@Test
+	public void inputMultiIntArraysOutputMultiIntArray() {
+		int[][] input1 = new int[][] { { 1, 2 }, { 3, 4 } };
+		int[][] input2 = new int[][] { { 1, 2 }, { 3, 4 } };
+		int[][] expected = new int[][] { { 2, 4 }, { 6, 8 } };
+		int[][] result = (int[][]) model.in("input1", input1).in("input2", input2).out("output").run()
+				.getIntArrayMultidimensional("output");
+		for (int i = 0; i < expected.length; i++) {
+			Assert.assertArrayEquals(expected[i], result[i]);
+		}
+	}
+
+	@Test
+	public void inputMultiFloatArraysOutputMultiFloatArray() {
+		float[][] input1 = new float[][] { { 1.0f, 2.0f }, { 3.0f, 4.0f } };
+		float[][] input2 = new float[][] { { 1.0f, 2.0f }, { 3.0f, 4.0f } };
+		float[][] expected = new float[][] { { 2.0f, 4.0f }, { 6.0f, 8.0f } };
+		float[][] result = (float[][]) model.in("input1", input1).in("input2", input2).out("output").run()
+				.getFloatArrayMultidimensional("output");
+		for (int i = 0; i < expected.length; i++) {
+			Assert.assertArrayEquals(expected[i], result[i], 0.0f);
+		}
+	}
+
+	@Test
+	public void inputMultiDoubleArraysOutputMultiDoubleArray() {
+		double[][] input1 = new double[][] { { 1.0d, 2.0d }, { 3.0d, 4.0d } };
+		double[][] input2 = new double[][] { { 1.0d, 2.0d }, { 3.0d, 4.0d } };
+		double[][] expected = new double[][] { { 2.0d, 4.0d }, { 6.0d, 8.0d } };
+		double[][] result = (double[][]) model.in("input1", input1).in("input2", input2).out("output").run()
+				.getDoubleArrayMultidimensional("output");
+		for (int i = 0; i < expected.length; i++) {
+			Assert.assertArrayEquals(expected[i], result[i], 0.0d);
+		}
+	}
+
+	@Test
+	public void inputMultiStringArraysOutputMultiStringArray() {
+		String[][] input1 = new String[][] { { "1", "2" }, { "3", "4" } };
+		String[][] input2 = new String[][] { { "1", "2" }, { "3", "4" } };
+		String[][] expected = new String[][] { { "2", "4" }, { "6", "8" } };
+		String[][] result = (String[][]) model.in("input1", input1).in("input2", input2).out("output").run()
+				.getStringArrayMultidimensional("output");
+		for (int i = 0; i < expected.length; i++) {
+			Assert.assertArrayEquals(expected[i], result[i]);
+		}
+	}
 }
