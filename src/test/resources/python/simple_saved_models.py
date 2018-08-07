120ebbd6a33d5d9d447ff646953ca9d5412c3642
Deron Eriksson <deron@us.ibm.com>
2018-08-07 12:24:35 -0700

    Create SavedModel for boolean logic

--- src/test/resources/python/simple_saved_models.py
+++ src/test/resources/python/simple_saved_models.py
@@ -120,9 +120,40 @@ def add_string():
                           outputs={"output": output})
 
 
+def boolean_logic():
+  tf.reset_default_graph()
+  sess = tf.Session()
+  input1 = tf.placeholder(tf.bool, name="input1")
+  input2 = tf.placeholder(tf.bool, name="input2")
+  output_and = tf.identity(input1 & input2, "output_and")
+  output_or = tf.identity(input1 | input2, "output_or")
+  output_not_and = tf.identity(~(input1 & input2), "output_and")
+  output_not_or = tf.identity(~(input1 | input2), "output_and")
+  output_xor = tf.identity(input1 ^ input2, "output_or")
+  print(input1)
+  print(input2)
+  print(output_and)
+  print(output_or)
+  print(output_not_and)
+  print(output_not_or)
+  print(output_xor)
+
+  dict = {input1: True, input2: False}
+  result = sess.run([output_and, output_or, output_not_and, output_not_or, output_xor], dict)
+  print(result)
+
+  saved_model.simple_save(sess,
+                          '/Users/deroneriksson/Documents/workspace5/tf-java-tryout/simple/boolean_logic',
+                          inputs={"input1": input1, "input2": input2},
+                          outputs={"output_and": output_and, "output_or": output_or,
+                                   "output_not_and": output_not_and, "output_not_or": output_not_or,
+                                   "output_xor": output_xor})
+
+
 if __name__ == "__main__":
   add_int32()
   add_int64()
   add_float32()
   add_float64()
   add_string()
+  boolean_logic()
